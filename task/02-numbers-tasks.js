9fd6b1520e0b08ee3796538a64fc8fb9cbf84153
MargaretOm <margosha20617@gmail.com>
2019-04-11 13:55:24 +0300

    My progress

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -165,7 +165,8 @@ function parseNumberFromString(value) {
  *   1,2,3   => 3.741657386773941
  */
 function getParallelipidedDiagonal(a,b,c) {
-    throw new Error('Not implemented');
+    return(Math.sqrt(Math.pow(a,2) + Math.pow(b,2) + Math.pow(c,2)));
+    //throw new Error('Not implemented');
 }
 
 /**
@@ -246,13 +247,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-    if (Number(value) == true){
-        return value;
-
-    } else {
-        return def;
-    }
-    //throw new Error('Not implemented');
+    throw new Error('Not implemented');
 }
 
 module.exports = {
